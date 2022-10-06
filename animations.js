8df76ca9a520fdd7c70efa514309b1d9da3c848d
Glitch (glitch-hello-website) <none>
2022-10-06 03:07:41 +0000

    📕🐄 Checkpoint ./animations.js:48994841/1374

--- animations.js
+++ animations.js
@@ -577,11 +577,48 @@ let animations = [
       //p.rect(40, 0, 40, 40);
 
       // Draw multiple circles in multiple locations
+      let count = 10;
+      let tileSize = p.width / count;
+
+      for (let i = 0; i < count; i++) {
+        let x = tileSize * (i + 0.5);
+        let y = p.height / 2;
+
+        let hue = i * 20 + t * 100;
+
+        hue %= 360; // Wrap the hue around 360 degrees, P5 can't handle >360 hues
+
+        // Reusing the hue allows us to make a dropshadow
+        //  and a highlight in the same color family
+
+        // Dropshadow
+        p.fill(hue, 100, 20);
+        p.noStroke();
+        p.ellipse(x, y + tileSize / 2, tileSize, tileSize * 0.5);
+
+        // Main circle
+        p.fill(hue, 100, 40);
+        p.stroke(hue, 100, 20);
+        p.circle(x, y, tileSize);
+
+        // Highlight
+        p.fill(hue, 100, 60);
+        p.noStroke();
+        p.circle(x - tileSize * 0.05, y - tileSize * 0.05, tileSize * 0.8);
+
+        p.fill(hue, 100, 80);
+        p.noStroke();
+        p.circle(x - tileSize * 0.1, y - tileSize * 0.1, tileSize * 0.5);
+      
+      
+      
+      
+      
       let x = pct * p.width;
       // x = 50
       let hue = pct * 360;
       p.textSize(100);
-      p.text("🌱", 100, 100);
+      //p.text("🌱", 100, 100);
       p.fill(hue, 100, 50);
       p.text("🍵", 100, 250);
 
@@ -593,7 +630,7 @@ let animations = [
       // //p.circle(x + p.width, 200, 100);
       // //p.circle(x - p.width, 100, 100);
 
-      //p.scale(1, Math.sin(pctTheta) * 0.2 + 1);
+      p.scale(1, Math.sin(pctTheta) * 0.2 + 1);
       //p.circle(x, 100, 100);
       p.textSize(56);
       p.text("🌱", 120, 150);
@@ -626,6 +663,8 @@ let animations = [
       //       p.pop()
 
       p.pop();
+
+      
     },
   },
   {
